attempting to fix memory overflow

multiparent_wrapper.py
@@ -79,9 +79,10 @@ class NeuralCrossoverWrapper(BeforeAfterPublisher):
             return
         
         if total_batches_length > self.batch_size:
-            self.batch_stack_fitness_values = self.batch_stack_fitness_values[-self.batch_size:]
-            self.sampled_solutions = self.sampled_solutions[-self.batch_size:]
-            self.sampled_action_space = self.sampled_action_space[-self.batch_size:]
+            to_remove = total_batches_length - self.batch_size
+            del self.batch_stack_fitness_values[:to_remove]
+            del self.sampled_solutions[:to_remove]
+            del self.sampled_action_space[:to_remove]
 
         if self.best_of_gen_callback is not None and self.fitness_epsilon > 0:
             best_batch_fitness = torch.max(torch.cat(self.batch_stack_fitness_values, dim=0).unsqueeze(1))